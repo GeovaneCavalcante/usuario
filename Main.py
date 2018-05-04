1aed70eef4da6debf67f9209cd356b6bde20c778
Geovane Cavalcante <geovanefeitosacavalcante@gmail.com>
2018-05-04 10:58:56 -0300

    Finalizado

--- Main.py
+++ Main.py
@@ -1,34 +1,47 @@
-from Usuario import Usuario
+import os, time
+from time import gmtime, strftime
+
 
 usuarios = []
 
 
-def main():
+def insert():
 
-    resposta = "s"
+    os.system('cls' if os.name == 'nt' else 'clear')
+    dados = {
+        "nome": str(input("Digite nome do usuario: ")),
+        "apelido": str(input("Digite nome do apelido: ")),
+        "cargo": str(input("Digite nome do cargo: "))
+    }
+    
+    escolhas = {
+        0: 'visitante',
+        1: 'usuario', 
+        2: 'adminsitrativo', 
+        3: 'técnico ', 
+        4: 'super usuario',
+    }
+    
+    print("\nEscolha um nível de acesso para usuário: ")
 
-    while(resposta == "s"):
+    for i in range(0, 4):
+        print(str(i) + ". " + escolhas.get(i))
 
-        dados = {
-            "nome": str(input("Digite nome do usuario: ")),
-            "apelido": str(input("Digite nome do apelido: ")),
-            "cargo": str(input("Digite nome do cargo: ")),
-            "nivel": str(input("Digite nome do nivel: ")) 
-        }
-        if(validate(dados)):
-            usuarios.append(dados)
-        else:
-            print("Apelido existente!")
+    op = int(input("\nDigite qual das opções: "))    
+    dados['nivel'] = escolhas.get(op)
 
+    if(validate(dados)):
+        dados['data_cadastro'] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        usuarios.append(dados)
         
-        print("\n")
+    else:
+        print("Apelido existente!")
 
-        for user in usuarios:
-            print(user)    
-
-        print("\n")
-
-        resposta = str(input("\n\nDeseja continuar?"))
+    print("\n")
+  
+    print('!!! Usuário inserido')
+    time.sleep(2)
+    main()
 
 
 def validate(dados):
@@ -38,5 +51,42 @@ def validate(dados):
             return False
 
     return True        
+
+
+def lista():
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Todos usuários")
+
+    for user in usuarios:
+        print(user)
+
+    time.sleep(5)
+    main()    
+
+
+def find():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    user_name = str(input("Digite o apelido do usuario que deseja buscar: "))
+    for user in usuarios:
+        if(user.get('apelido') == user_name):
+            print(user)
+            
+
+def main():
+    
+    opcoes = 0
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("1. para inserir um usuário: ")
+    print("2. para listar todos usuários: ")
+    print("3. para buscar um usuário: ")
+    opcoes = int(input("\nDigite a opção: "))
+    
+    if(opcoes == 1):
+        insert()  
+    elif(opcoes == 2): 
+        lista()   
+    else:
+        find()    
 
 main()
